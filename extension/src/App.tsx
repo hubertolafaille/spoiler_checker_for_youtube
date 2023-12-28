1b2d9ac3a64905355f973390e51e1170213dbadc
Huberto Lafaille <120303136+hubertolafaille@users.noreply.github.com>
2023-12-28 05:28:12 +0100

    Tailwind CSS and ShadCN added

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css'
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
 
 
     return (
         <>
-
+            <div className={"flex justify-center items-center h-screen m-auto"}>
+                <Button>Click me</Button>
+            </div>
         </>
     )
 }
